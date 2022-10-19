274f27df17ecbb1e5bb2730a21ea5d0e4fa17f94
Vahlehrie <vahlehrie@gmail.com>
2022-10-18 20:52:21 -0700

    task one

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*prints_alphabets -> lower case
+*/
+
+void print_alphabet_x10(void);
+{
+int j;
+int p;
+
+for (p = '0' ; p < 10 ; j++)
+{
+for (j = 'a' ; j <= 'z' ; j++)
+{
+_putchar(j);
+}
+_putchar('\n'),
+}
+}
+
